905c6e07632168f24317907b28eaa8479a961204
vishal kumar <130959006+Vishalpandey1799@users.noreply.github.com>
2025-09-17 21:33:55 +0530

    project setup

---
+++ Server/index.js
@@ -0,0 +1,30 @@
+console.log("🚀 Welcome to Lumo AI Backend");
+
+import express from "express";
+import { configDotenv } from "dotenv";
+import cors from "cors";
+
+configDotenv();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+// Enable CORS
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
+
+// Enable JSON parsing for all routes
+app.use(express.json({ limit: "10mb" }));
+
+// Example health check route
+app.get("/health", (req, res) => {
+    res.send({ message: "Lumo AI Backend is running!" });
+});
+
+ 
+
+app.listen(PORT, () => {
+    console.log(`✅ Server is running on http://localhost:${PORT}`);
+});
